d3543006d1b1c48c888b9e091185fb5c9a4cf694
Ian Page Hands <ihands@redhat.com>
2014-09-10 10:45:31 -0400

    toying with blur

--- generate.sh
+++ generate.sh
@@ -10,22 +10,27 @@ then
 	exit
 fi
 
+rm output*stl
+
 echo "finding appropriate size"
 for var in `seq 1 50`
 do
 	echo $var
 	convert $IMG -resize ${var}% -background black -vignette 50x5000 -normalize $TMP_IMG
-	python lib/stl_tools_helper.py  output.0.028.stl 0.028
 
-	size=`ls -l output.0.028.stl | awk '{print $5}'`
+	python lib/stl_tools_helper.py output.stl 0.025 1
+
+	size=`ls -l output.stl | awk '{print $5}'`
 	if [ $size -gt 4000000 ]
 	then
 		echo "correct size is ${var}%"
-		for scale in 0.020 0.035 0.040 0.050
+
+		for blur in 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0
 		do
-			echo "generating STL $scale"
-			python lib/stl_tools_helper.py output.${scale}.stl ${scale}
+			echo "generating STL with blur $blur"
+			python lib/stl_tools_helper.py output.${blur}.stl 0.025 ${blur}
 		done
+
 		break
 	fi
 done
