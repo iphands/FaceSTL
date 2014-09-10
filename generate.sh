3a1b50d8f1a4e77171019f8c696ddb62c4fa7ccc
Ian Page Hands <ihands@redhat.com>
2014-09-10 09:51:41 -0400

    generate multiple scaled stls

--- generate.sh
+++ generate.sh
@@ -10,14 +10,24 @@ then
 	exit
 fi
 
-for var in `seq 10 100`
+echo "finding appropriate size"
+for var in `seq 1 100`
 do
 	echo $var
 	convert $IMG -resize ${var}% $TMP_IMG
-	python lib/stl_tools_helper.py
-	size=`ls -l output.stl | awk '{print $5}'`
+	python lib/stl_tools_helper.py  output.0.028.stl 0.028
+
+	ls -lh *stl
+
+	size=`ls -l output.0.028.stl | awk '{print $5}'`
 	if [ $size -gt 4000000 ]
 	then
+		echo "correct size is ${var}%"
+		for scale in 0.020 0.035 0.040 0.050
+		do
+			echo "generating STL $scale"
+			python lib/stl_tools_helper.py output.${scale}.stl ${scale}
+		done
 		break
 	fi
 done
